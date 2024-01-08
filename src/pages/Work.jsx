97570ba7bca9395b3dff050cff5e6e8156707343
Prateek <prateeksagar806@gmail.com>
2024-01-08 12:35:45 +0530

    Inlcuding framer motion for scrolling effect in the work section and changes made in work.jsx according to it with optimize the code

--- src/pages/Work.jsx
+++ src/pages/Work.jsx
@@ -1,5 +1,6 @@
-import React, { useState } from "react";
-import workBg from "../assets/workBackground.jpg";
+import React, { useRef, useState } from "react";
+import { motion, useTransform, useScroll } from "framer-motion";
+import workBg from "../assets/slider_image_2.jpg";
 import work1 from "../assets/works/orderWork1.jpeg";
 import work2 from "../assets/works/orderWork2.jpeg";
 import work3 from "../assets/works/orderWork3.jpeg";
@@ -7,10 +8,70 @@ import work4 from "../assets/works/orderWork4.jpeg";
 import work5 from "../assets/works/orderWork5.jpeg";
 import work6 from "../assets/works/SelfWork1.jpeg";
 import ImageShow from "./components/ImageShow";
-
+const HorizontalScroll = (props) => {
+  const targetRef = useRef(null);
+  const { scrollYProgress } = useScroll({
+    target: targetRef,
+  });
+  console.log(scrollYProgress);
+  const x =
+    window.innerWidth < 640
+      ? useTransform(scrollYProgress, [0, 1], ["1%", "-240%"])
+      : window.innerWidth < 1040
+      ? useTransform(scrollYProgress, [0, 1], ["1%", "-100%"])
+      : useTransform(scrollYProgress, [0, 1], ["1%", "-50%"]);
+  const img_display = props.display.map(({ id, value }) => (
+    <img
+      src={value}
+      key={id}
+      className="w-auto min-h-[60vh] md:h-[300px] object-cover rounded-lg shadow-lg shadow-black"
+    />
+  ));
+  return (
+    <section ref={targetRef} className="relative h-[200dvh]">
+      <div className="sticky top-36 md:top-32 my-4 h-fit flex items-center justify-center overflow-hidden">
+        <motion.div
+          style={{ x }}
+          className="flex justify-between gap-4 w-[200vw] my-4 overflow-visible"
+        >
+          {img_display}
+        </motion.div>
+      </div>
+    </section>
+  );
+};
 export default function Work() {
   const [showImageModal, setShowImageModal] = useState(false);
   const [currentImage, setCurrentImage] = useState(null);
+
+  const section_one = [
+    {
+      id: 1,
+      value: work1,
+    },
+    {
+      id: 2,
+      value: work2,
+    },
+    {
+      id: 3,
+      value: work3,
+    },
+  ];
+  const section_two = [
+    {
+      id: 1,
+      value: work4,
+    },
+    {
+      id: 2,
+      value: work5,
+    },
+    {
+      id: 3,
+      value: work6,
+    },
+  ];
   const openImage = (event) => {
     setCurrentImage(event);
     setShowImageModal(true);
@@ -18,89 +79,53 @@ export default function Work() {
   const handleClose = () => {
     setShowImageModal(!showImageModal);
   };
-  return (
+  const features = [
+    {
+      id: 1,
+      value: "Works",
+      _top: "0vh",
+    },
+    {
+      id: 2,
+      value: "Different type of work",
+      _top: "80vh",
+    },
+    {
+      id: 3,
+      value: "Perfect gift for anyone",
+      _top: "170vh",
+    },
+    {
+      id: 4,
+      value: "Ontime Delivery",
+      _top: "330vh",
+    },
+  ];
+  const featuresShow = features.map(({ id, value, _top }) => (
     <div
-      id="work"
       style={{
-        backgroundImage: `url(${workBg})`,
-        backgroundPosition: "center",
-        backgroundSize: "cover",
+        top: _top,
+        WebkitTextStroke: "2px black",
       }}
-      className="w-full min-h-screen h-[880px] relative bg-neutral-200 flex flex-col"
+      className="w-full text-white flex justify-start md:justify-center items-center overflow-hidden absolute text-[62px] md:text-[104px] font-black font-roboto z-[2]"
+      key={id}
     >
-      <div className="h-full mx-10 md:mx-[80px] py-4 xl:py-8 2xl:mx-[240px]">
-        <div className="w-24 h-10 bg-cyan-600 rounded-full flex justify-center items-center cursor-pointer py-4 top-4 right-10 md:right-[80px] 2xl:right-[240px] hover:scale-125 duration-300 absolute">
-          <p className="text-white text-sm font-bold font-roboto">Order Now</p>
-        </div>
+      {value}
+    </div>
+  ));
 
-        <div className="w-full h-[10%] flex flex-col md:flex-row items-center">
-          {/* heading and button */}
-          <div className="w-full h-full flex justify-center items-center my-2 text-black text-[48px] lg:text-[64px] font-bold font-roboto">
-            Works
-          </div>
-        </div>
+  return (
+    <div id="work" className="w-full min-h-screen relative flex flex-col">
+      <div className="absolute -z-[1] bg-black w-full h-full"></div>
+
+      <div className="h-full mx-10 py-4 xl:py-8 sticky top-20">
+        {featuresShow}
         {/* work gallery */}
-        <div className="h-[90%] max-h-[720px] my-10">
-          {/* section 1 */}
-          <div className="w-full h-1/2 relative flex justify-center">
-            <div className="w-2/5 h-4/5 bg-neutral-600 overflow-hidden rounded-[10px] left-0 hover:scale-105 duration-300 absolute cursor-pointer z-[2] hover:z-10">
-              <img
-                onClick={() => {
-                  openImage(work1);
-                }}
-                src={work1}
-                className="w-full h-full object-cover"
-              />
-            </div>
-            <div className="w-2/5 h-4/5 bg-neutral-400 overflow-hidden rounded-[10px] top-10 hover:scale-105 duration-300 absolute cursor-pointer z-[4] hover:z-10">
-              <img
-                onClick={() => {
-                  openImage(work2);
-                }}
-                src={work2}
-                className="w-full h-full object-cover"
-              />
-            </div>
-            <div className="w-2/5 h-4/5 bg-neutral-600 overflow-hidden rounded-[10px] right-0 hover:scale-105 duration-300 absolute cursor-pointer z-[2] hover:z-10">
-              <img
-                onClick={() => {
-                  openImage(work4);
-                }}
-                src={work4}
-                className="w-full h-full object-cover"
-              />
-            </div>
-          </div>
-          {/* Section 2 */}
-          <div className="w-full h-1/2 flex justify-center relative">
-            <div className="w-2/5 h-4/5 bg-neutral-600 overflow-hidden rounded-[10px] left-0 hover:scale-105 duration-300 absolute cursor-pointer z-[2] hover:z-10 flex justify-center items-center">
-              <img
-                onClick={() => {
-                  openImage(work3);
-                }}
-                src={work3}
-                className="w-full h-full object-cover"
-              />
-            </div>
-            <div className="w-2/5 h-4/5 bg-neutral-400 overflow-hidden rounded-[10px] top-10 hover:scale-105 duration-300 absolute cursor-pointer z-[4] hover:z-10">
-              <img
-                onClick={() => {
-                  openImage(work5);
-                  showImageModal(true);
-                }}
-                src={work5}
-                className="w-full h-full object-cover"
-              />
-            </div>
-            <div className="w-2/5 h-4/5 bg-neutral-600 overflow-hidden rounded-[10px] right-0 hover:scale-105 duration-300 absolute cursor-pointer z-[2] hover:z-10">
-              <img
-                onClick={() => {
-                  openImage(work6);
-                }}
-                src={work6}
-                className="w-full h-full object-cover"
-              />
-            </div>
+        <HorizontalScroll display={section_one} />
+        <HorizontalScroll display={section_two} />
+        <div className="w-full h-10  rounded-full flex justify-center items-center">
+          <div className="text-white bg-cyan-600 rounded-full h-full w-24 text-sm flex justify-center items-center font-bold font-roboto cursor-pointer hover:scale-125 duration-300">
+            Order Now
           </div>
         </div>
       </div>
